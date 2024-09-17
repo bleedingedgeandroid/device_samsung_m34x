0da792e10a51c517fa7af9a04e50245108839f45
PugzAreCute <me@pugzarecute.com>
2024-09-17 10:01:58 +0530

    use samsung health HAL

--- device.mk
+++ device.mk
@@ -18,8 +18,8 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.samsung \
+    android.hardware.health-service.samsung-recovery 
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
